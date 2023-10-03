fe8203afaf3567eb90fe640abe4c9c0263a993ac
Mikko Suokas <121874134+Hienomekaanikko@users.noreply.github.com>
2023-10-03 10:01:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,30 +4,30 @@ import io
 
 app = Flask(__name__)
 
-# Define a function to filter and sum amounts by "Otsikko"
 def filter_and_sum_by_otsikko(rows):
     result_dict = {}
-    
-    # Skip the header row
     header = rows[0]
-    
+
     for row in rows[1:]:
         kirjauspaiva, maara, maksaja, maksunsaaja, nimi, otsikko, viitenumero, valuutta = row
-        
-        # Convert the "Maara" (amount) to a float and handle possible empty strings
         try:
             maara = float(maara.replace(',', '.'))
         except ValueError:
             maara = 0.0
-        
+
         if otsikko in result_dict:
-            result_dict[otsikko] += maara
+            result_dict[otsikko]['maara'] += maara
         else:
-            result_dict[otsikko] = maara
-    
-    # Convert the result dictionary to a list of tuples for easy formatting
-    result_list = [(otsikko, sum) for otsikko, sum in result_dict.items()]
-    
+            result_dict[otsikko] = {
+                'kirjauspäivä': kirjauspaiva,
+                'otsikko': otsikko,
+                'maara': maara
+            }
+
+    result_list = [(event['kirjauspäivä'], event['otsikko'], event['maara']) for event in result_dict.values()]
+
+    result_list.sort(key=lambda x: x[2])  
+
     return result_list
 
 @app.route('/')
@@ -39,30 +39,30 @@ def upload():
     files = request.files.getlist('csv_files')
     result_messages = []
 
-    csv_data = []
-    
     for file in files:
         try:
-            # Check if the file is a CSV
             if file.filename.endswith('.csv'):
-                # Read CSV data
                 csv_data = []
                 with file.stream as csv_file:
-                    # Read the file content as text with the appropriate encoding
+                
                     file_content = csv_file.read().decode('utf-8')
-                    
-                    # Create a file-like object for the CSV reader
+             
                     csv_file_object = io.StringIO(file_content)
                     
                     csv_reader = csv.reader(csv_file_object, delimiter=';', quotechar='"')
                     for row in csv_reader:
                         csv_data.append(row)
 
-                # Extract and sum amounts by "Otsikko"
                 filtered_and_summed_data = filter_and_sum_by_otsikko(csv_data)
 
-                result_messages.append(f'File: {file.filename}')
-                result_messages.append(f'Sum of Amounts by "Otsikko":\n{filtered_and_summed_data}')
+                result_message = f'File: {file.filename}\n'
+                result_message += 'Sum of Amounts by "Otsikko":\n'
+                for event in filtered_and_summed_data:
+                    result_message += f'Kirjauspäivä: {event[0]}\n'
+                    result_message += f'Otsikko: {event[1]}\n'
+                    result_message += f'Määrä: {event[2]} EUR\n\n'
+
+                result_messages.append(result_message)
             else:
                 result_messages.append(f'Error: {file.filename} is not a CSV file')
         except Exception as e:
